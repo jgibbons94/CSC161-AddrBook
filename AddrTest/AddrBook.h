5d2d8f139c774aca8caed990ff584e1416a3f819
Jesse Gibbons <jessegibbons@outlook.com>
2016-09-19 11:02:10 -0600

    Added AddrBook.h header file.

--- AddrTest/AddrBook.h
+++ AddrTest/AddrBook.h
@@ -1,5 +1,18 @@
 namespace AddrBook
 {
+	/*
+		Design and implement the AddrBook data structure class (collection class)
+		A bag collection of Contact objects that uses a static array as the underlying data structure (set to fixed size of 5).
+		Required member functions:
+		void AddContact(Contact itemToAdd);
+		void RemoveContact(Contact itemToRemove);
+		int FindContact(Contact itemToFind);
+		void RemoveContactByIndex(int indexToRemove);
+		void PrintAllContacts();
+		int GetUsed();
+		void ReadFile(string fileName);
+		void WriteFile(string fileName);
+	*/
 	class AddrBook
 	{
 	public:

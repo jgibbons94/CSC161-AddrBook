4bd1ed024c57faa0780e728388c6f604be13d518
Jesse Gibbons <jessegibbons@outlook.com>
2016-09-19 10:30:46 -0600

    Implemented the Adress class

--- AddrTest/Address.cpp
+++ AddrTest/Address.cpp
@@ -1,9 +1,35 @@
 #include "Address.h"
+#include <iostream>
 
 
-
-Address::Address()
+AddrBook::Address::Address(std::string initialStreetAddress, std::string initialCity, std::string initialState, std::string initialZip)
 {
+	streetAddress = initialStreetAddress;
+	city = initialCity;
+	state = initialState;
+	zip = initialZip;
 }
 
+std::string AddrBook::Address::ToString() const
+{
+	return streetAddress + '\n' + city + ", " + state + ' ' + zip;
+}
 
+std::string AddrBook::Address::ToFileString(char delimeter) const
+{
+	return streetAddress + delimeter + city + delimeter + state + delimeter + zip;
+}
+
+std::string AddrBook::Address::ConsoleInput() 
+{
+	using namespace std;
+	cout << "Please enter a street address: ";
+	cin >> streetAddress;
+	cout << "Please enter a city: ";
+	cin >> city;
+	cout << "Please enter a state: ";
+	cin >> zip;
+	cout << "Please enter a zip code: ";
+	cin >> zip;
+	return std::string();
+}
--- AddrTest/Address.h
+++ AddrTest/Address.h
@@ -1,9 +1,49 @@
+#ifndef ADDRESS_H
+#define ADDRESS_H
+#include <string>
 namespace AddrBook
 {
+	/*
+		Address class
+		File names: Address.(h and cpp)
+		Data members: streetAddress as string, city as string, state as string and zip as string
+		Private data and public get and set member functions
+		Default constructors and constructors that take parameters
+		ToString member function formatted for output window: string ToString();
+		ToFileString member function formatted for file: string ToFileString();
+		ConsoleInput member function (extract from keyboard): void ConsoleInput();
+		Implemented with a unique macro guard
+*/
 	class Address
 	{
 	public:
-		Address();
+
+		//constructor:
+		Address(std::string initialStreetAddress = "", std::string initialCity = "", std::string initialState = "", std::string initialZip = "");
+
+		//Get methods:
+		std::string GetStreetAddress() const { return streetAddress; }
+		std::string GetCity() const { return city; }
+		std::string GetState() const { return state; }
+		std::string GetZip() const { return zip; }
+
+		//Set methods:
+		void setStreetAddress(std::string newStreetAddress) { streetAddress = newStreetAddress; }
+		void setCity(std::string newCity) { city = newCity; }
+		void setState(std::string newState) { state = newState; }
+		void setZip(std::string newZip) { zip = newZip; }
+
+		//Member functions:
+		std::string ToString() const;
+		std::string ToFileString(char delimeter = ',') const;
+		std::string ConsoleInput();
+
+	private:
+		std::string streetAddress;
+		std::string city;
+		std::string state;
+		std::string zip;
 	};
 
 }
+#endif

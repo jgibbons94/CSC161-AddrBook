70cd1af5c7f4ea796d3805755484783e3c75dec0
Jesse Gibbons <jessegibbons@outlook.com>
2016-09-19 10:33:12 -0600

    Implemented the Contact class

--- AddrTest/Contact.cpp
+++ AddrTest/Contact.cpp
@@ -1,10 +1,39 @@
 #include "Contact.h"
 using namespace AddrBook;
 
-
-
-Contact::Contact()
+AddrBook::Contact::Contact(Name initialName, Address initialAddress, std::string initialPhone, std::string initialEmail, std::string initialBirthday, std::string initialPictureFile)
 {
+	fullName = initialName;
+	fullAddress = initialAddress;
+	phone = initialPhone;
+	email = initialEmail;
+	birthday = initialBirthday;
+	pictureFile = initialPictureFile;
 }
 
+std::string AddrBook::Contact::ToString() const
+{
+	return fullName.ToString() + '\n'
+		+ fullAddress.ToString() + '\n'
+		+ phone + '\n'
+		+ email + '\n'
+		+ birthday + '\n'
+		+ pictureFile + '\n';
+}
 
+std::string AddrBook::Contact::ToFileString(char delimeter) const
+{
+	return fullName.ToFileString(delimeter) + delimeter
+		+ fullAddress.ToFileString(delimeter) + delimeter
+		+ phone + delimeter
+		+ email + delimeter
+		+ birthday + delimeter
+		+ pictureFile;
+}
+
+std::string AddrBook::Contact::ConsoleInput()
+{
+	fullName.ConsoleInput();
+	fullAddress.ConsoleInput();
+
+}
--- AddrTest/Contact.h
+++ AddrTest/Contact.h
@@ -1,9 +1,58 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <string>
+#include "Name.h"
+#include "Address.h"
 namespace AddrBook
 {
+	/*
+		Contact class
+		File names: Contact.(h and cpp)
+		Data members: fullName as Name object, fullAddress as Address object, phone as string, email as string, birthday as string and pictureFile as string
+		Private data and public get and set member functions
+		Default constructors and constructors that take parameters
+		ToString member function formatted for output window: string ToString();
+		ToFileString member function formatted for file: string ToFileString();
+		ConsoleInput member function (extract from keyboard): void ConsoleInput();
+		Implemented with a unique macro guard
+	*/
 	class Contact
 	{
 	public:
-		Contact();
+
+		//constructor:
+		Contact(Name initialName = Name(), Address initialAddress = Address(), std::string initialPhone = "", std::string initialEmail = "", std::string initialBirthday = "", std::string initialPictureFile = "");
+
+		//Get methods:
+		Name GetFullName() const { return fullName; }
+		Address GetFullAddress() const { return fullAddress; }
+		std::string GetPhone() const { return phone; }
+		std::string GetEmail() const { return email; }
+		std::string GetBirthday() const { return birthday; }
+		std::string GetPictureFile() const { return pictureFile; }
+
+		//Set methods:
+		void SetFullName(Name newName) { fullName = newName; }
+		void SetFullAddress(Address newAddress) { fullAddress = newAddress; }
+		void SetPhone(std::string newPhone) { phone = newPhone; }
+		void SetEmail(std::string newEmail) { email = newEmail; }
+		void SetBirthday(std::string newBirthday) { birthday = newBirthday; }
+		void SetPictureFile(std::string newPicFile) { pictureFile = newPicFile; }
+
+		//Member functions:
+
+		std::string ToString() const;
+		std::string ToFileString(char delimeter = ',') const;
+		std::string ConsoleInput();
+
+	private:
+		Name fullName;
+		Address fullAddress;
+		std::string phone;
+		std::string email;
+		std::string birthday;
+		std::string pictureFile;
 	};
 
 }
+#endif
